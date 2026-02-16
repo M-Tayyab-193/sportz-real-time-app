f06e1b063b6e5b141680db0320f668aab365aedd
MTayyab193 <tayyabprince193@gmail.com>
2026-02-16 11:50:33 +0500

    Add "matches" REST API, Change Neon DB Driver

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import express from 'express';
+import express from "express";
+import matchRouter from "#routes/matches.routes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({message: "Hello World!"});
-})
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the Sportz API!" });
+});
+app.use("/matches", matchRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port http://localhost:${PORT}`);
-})
+  console.log(`Server is listening on port http://localhost:${PORT}`);
+});
 
 export default app;
---
+++ src/routes/matches.routes.js
@@ -0,0 +1,71 @@
+import express from "express";
+import {
+  createMatchSchema,
+  listMatchesQuerySchema,
+} from "#validation/matches.js";
+import { db } from "#db/db.ts";
+import { getMatchStatus } from "#utils/match-status.js";
+import { matches } from "#db/schema.ts";
+import { desc } from "drizzle-orm";
+const matchRouter = express.Router();
+
+matchRouter.get("/", async (req, res) => {
+  const parsed = listMatchesQuerySchema.safeParse(req.query);
+  if (!parsed.success) {
+    return res.status(400).json({
+      message: "Invalid query parameters",
+      error: JSON.stringify(parsed.error),
+    });
+  }
+  const limit = Math.min(parsed.data.limit ?? 50, 100);
+  try {
+    const data = await db
+      .select()
+      .from(matches)
+      .orderBy(desc(matches.createdAt))
+      .limit(limit);
+
+    res.json({ data });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to fetch matches",
+      error: JSON.stringify(error),
+    });
+  }
+});
+
+matchRouter.post("/", async (req, res) => {
+  const parsed = createMatchSchema.safeParse(req.body);
+
+  if (!parsed.success) {
+    return res.status(400).json({
+      message: "Invalid request body",
+      error: JSON.stringify(parsed.error),
+    });
+  }
+  const { data: { startTime, endTime, homeScore, awayScore } = {} } = parsed;
+
+  try {
+    const [event] = await db
+      .insert(matches)
+      .values({
+        ...parsed.data,
+        startTime: new Date(startTime),
+        endTime: new Date(endTime),
+        homeScore: homeScore || 0,
+        awayScore: awayScore || 0,
+        status: getMatchStatus(startTime, endTime),
+      })
+      .returning();
+    res
+      .status(201)
+      .json({ message: "Match created successfully", data: event });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to create a match",
+      error: JSON.stringify(error),
+    });
+  }
+});
+
+export default matchRouter;
